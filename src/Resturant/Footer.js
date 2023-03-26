1c3116d52909b56e7e75d7ac722cb5d10cddec22
sindhaya <119623899+sindhaya@users.noreply.github.com>
2023-03-26 22:57:25 +0530

    Add files via upload

---
+++ src/Resturant/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <button style={{padding:"10px", border:"2px solid blue", background:"violet"}}>swiggy</button>
+
+    </div>
+  )
+}
+
+export default Footer
